cc4cf36fb46e2fd944da90cd5611e754cf3c4122
Yash19k <kshatriyayash19@gmail.com>
2026-08-16 01:47:26 +0530

    Ribbon

--- src/components/hero/CombinedHeroNew.tsx
+++ src/components/hero/CombinedHeroNew.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { ReferenceHeroText } from "./ReferenceHeroText";
 import ReferenceImageParticles from "./ReferenceImageParticles";
 import MoltenMetal from "./MoltenMetal";
+import Ribbons from "../ui/Ribbons";
 import yashImg from "./yash.png";
 
 export const CombinedHeroNew: React.FC = () => {
@@ -48,6 +49,34 @@ export const CombinedHeroNew: React.FC = () => {
           mouseInteraction={true}
           mouseStrength={0.2}
           opacity={0.85}
+        />
+      </div>
+
+      {/* 0.1 Ribbons background layer */}
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          position: "absolute",
+          left: 0,
+          top: 0,
+          zIndex: 0,
+          pointerEvents: "none"
+        }}
+      >
+        <Ribbons
+          colors={['#8B5CF6']}
+          baseSpring={0.03}
+          baseFriction={0.92}
+          baseThickness={7}
+          offsetFactor={0}
+          maxAge={600}
+          pointCount={60}
+          speedMultiplier={0.3}
+          enableFade={true}
+          enableShaderEffect={true}
+          effectAmplitude={1.2}
+          backgroundColor={[0, 0, 0, 0]}
         />
       </div>
 
